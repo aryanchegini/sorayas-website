0b997dd7749e3f89879e5703b22e8f371872651f
Aryan Chegini <cheginiaryan.ac@gmail.com>
2023-11-15 16:10:45 +0000

    midway

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-window.addEventListener('load', resizeHomePage);
-window.addEventListener('resize', resizeHomePage);
-const nav = document.querySelector('.nav-div');
-const homeIntro = document.querySelector('.section-1');
+// window.addEventListener('load', resizeHomePage);
+// window.addEventListener('resize', resizeHomePage);
+// const nav = document.querySelector('nav');
+// const homeIntro = document.querySelector('.section-1');
+// const introImage = document.querySelector('.intro-image');
 
-function resizeHomePage(event) {
-    if (window.innerHeight > nav.clientHeight + homeIntro.clientHeight) {
-        homeIntro.style.height = (window.innerHeight-nav.clientHeight) + 'px';
-    }
-    
-}
+// function resizeHomePage(event) {
+//     if (window.innerHeight > nav.clientHeight + homeIntro.clientHeight && (introImage.clientHeight / homeIntro.style.height) > 0.8) {
+//         homeIntro.style.height = (window.innerHeight-nav.clientHeight) + 'px';
+//     }
+
+// }
