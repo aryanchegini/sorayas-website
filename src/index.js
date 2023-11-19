6c5d558077cbd96f46144eba155de841116c0283
Aryan Chegini <cheginiaryan.ac@gmail.com>
2023-11-19 12:00:49 +0000

    Implemented mobile navbar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-// window.addEventListener('load', resizeHomePage);
-// window.addEventListener('resize', resizeHomePage);
-// const nav = document.querySelector('nav');
-// const homeIntro = document.querySelector('.section-1');
-// const introImage = document.querySelector('.intro-image');
+const menuToggle = document.querySelector('.menu-toggle');
+const mobileNav = document.querySelector('.mobile-nav');
 
-// function resizeHomePage(event) {
-//     if (window.innerHeight > nav.clientHeight + homeIntro.clientHeight && (introImage.clientHeight / homeIntro.style.height) > 0.8) {
-//         homeIntro.style.height = (window.innerHeight-nav.clientHeight) + 'px';
-//     }
+menuToggle.addEventListener('click', (event) => {
+    console.log('hello');
+    mobileNav.classList.add('mobile-nav-active');
+    setTimeout(() => {
+        window.addEventListener('click', hideNavbar);
+    }, 250);
+});
 
-// }
+function hideNavbar() {
+    mobileNav.classList.remove('mobile-nav-active');
+    window.removeEventListener('click', hideNavbar);
+}
+
