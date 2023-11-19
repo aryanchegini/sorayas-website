e72437669546e929a8128ccce2db79a17d1d00aa
Aryan Chegini <cheginiaryan.ac@gmail.com>
2023-11-19 12:28:41 +0000

    Enlarged text in mobile menu; cleaned up source code;

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const mobileNav = document.querySelector('.mobile-nav');
+const menuToggle = document.querySelector(".menu-toggle");
+const mobileNav = document.querySelector(".mobile-nav");
 
-menuToggle.addEventListener('click', (event) => {
-    console.log('hello');
-    mobileNav.classList.add('mobile-nav-active');
-    setTimeout(() => {
-        window.addEventListener('click', hideNavbar);
-    }, 250);
+menuToggle.addEventListener("click", (event) => {
+  console.log("hello");
+  mobileNav.classList.add("mobile-nav-active");
+  setTimeout(() => {
+    window.addEventListener("click", hideNavbar);
+  }, 250);
 });
 
 function hideNavbar() {
-    mobileNav.classList.remove('mobile-nav-active');
-    window.removeEventListener('click', hideNavbar);
+  mobileNav.classList.remove("mobile-nav-active");
+  window.removeEventListener("click", hideNavbar);
 }
-
